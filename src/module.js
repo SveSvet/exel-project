86bc37f3a48a7705bd1814191a5d2d98343feb91
Svetlana <>
2022-11-04 22:13:39 +0300

    Webpack: add webpack config and eslint

--- src/module.js
+++
@@ -1,9 +0,0 @@
-const generateGreet = (name) => {
-   return (hello) => {
-     return `${hello}, ${name}`;
-  }
-};
-
-const helloSvetlana = generateGreet('Svetlana');
-helloSvetlana('Hi');
-console.log(helloSvetlana('Привет'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,95 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
 const path = require('path');
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
 // __dirname - путь до текущей директории
-// path.resolve(__dirname, 'src') - конкатенируем текущую директорию с папкой с именем src
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  entry: {
-    main: [
-      'core-js/stable',
-      'regenerator-runtime/runtime',
-      './index.js'
-    ]
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js'
-  },
-  resolve: {
-    extensions: ['.js', '.html'],
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src', 'core'),
+// path.resolve(__dirname, 'src') -
+// конкатенируем текущую директорию с папкой с именем src
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+  const isDev = !isProd;
+
+  const filename = (ext) => isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`;
+
+  const plugins = () => {
+    const base = [
+      new MiniCssExtractPlugin({
+        filename: filename('css'),
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new CopyPlugin({
+        patterns: [
+          {from: path.resolve(__dirname, 'src', 'favicon.ico'),
+            to: path.resolve(__dirname, 'dist'),
+          },
+        ],
+      }),
+    ];
+
+    if (isDev) {
+      base.push(new ESLintPlugin());
     }
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].bundle.css',
-    }),
-    new HtmlWebpackPlugin({
-      template: "./index.html"
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: path.resolve(__dirname, 'src', 'favicon.ico'),
-          to: path.resolve(__dirname, 'dist')
+
+    return base;
+  };
+
+  return {
+    target: 'web',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: [
+        'core-js/stable',
+        'regenerator-runtime/runtime',
+        './index.js',
+      ],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: filename('js'),
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.js', '.html'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', 'core'),
+      },
+    },
+    devServer: {
+      port: '3000',
+      open: true,
+      hot: true,
+      watchFiles: './',
+    },
+    devtool: isDev ? 'source-map' : false,
+    plugins: plugins(),
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
         },
       ],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ],
-  },
-
-}
+    },
+  };
+};
